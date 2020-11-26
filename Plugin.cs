6160bb168dc747d5326b2032c48bc73316cf0f46
xRoier <58123692+xRoier@users.noreply.github.com>
2020-11-26 16:28:48 +0100

    Version bump

--- Plugin.cs
+++ Plugin.cs
@@ -10,7 +10,7 @@ namespace SCP1162_EXI_2._0
         public override string Name => "SCP1162";
         public override string Author => "xRoier";
         public EventHandlers EventHandlers;
-        public override Version Version { get; } = new Version(2, 1, 0);
+        public override Version Version { get; } = new Version(2, 1, 1);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 18);
         public override void OnEnabled()
         {
