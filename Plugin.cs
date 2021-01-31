f3c4c6bfd2c63e9867f8eb70c68254ec689aa73f
xRoier <58123692+xRoier@users.noreply.github.com>
2021-01-31 12:22:09 +0100

    Yes

--- Plugin.cs
+++ Plugin.cs
@@ -10,21 +10,13 @@ namespace SCP1162_EXI_2._0
         public override string Name => "SCP1162";
         public override string Author => "xRoier";
         public EventHandlers EventHandlers;
-        public override Version Version { get; } = new Version(2, 1, 1);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 18);
+        public override Version Version { get; } = new Version(2, 1, 2);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 19);
         public override void OnEnabled()
         {
             base.OnEnabled();
             EventHandlers = new EventHandlers(this);
             EvPlayer.ItemDropped += EventHandlers.OnItemDropped;
-            try
-            {
-                Config.ParseChances();
-            }
-            catch(Exception e)
-            {
-                Log.Error($"Item chances parsing fucked up: {e.StackTrace}");
-            }
         }
         public override void OnDisabled()
         {
