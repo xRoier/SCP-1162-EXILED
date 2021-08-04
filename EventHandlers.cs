fb5b9f11110bfe59ea2dc1e72bc0f4d38b0eeae9
xRoier <58123692+xRoier@users.noreply.github.com>
2021-08-04 22:56:46 +0200

    Testing, I repeat, testing.

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -12,7 +12,7 @@ namespace SCP1162_EXI_2._0
         internal void OnItemDropped(ItemDroppedEventArgs ev)
         {
             if (Vector3.Distance(ev.Player.Position, RoleType.Scp173.GetRandomSpawnPoint()) <= 8.2f)
-            {
+            { 
                 if (plugin.Config.UseHints)
                     ev.Player.ShowHint(plugin.Config.ItemDropMessage, plugin.Config.ItemDropMessageDuration);
                 else
