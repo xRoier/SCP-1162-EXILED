17b5c87a4b500e1258eaa7d79ba2f1cdc650dec6
xRoier <rogerazomar@gmail.com>
2021-08-23 18:01:09 +0200

    Updated to EXILED 3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("SCP1162-EXI-2.0")]
+[assembly: AssemblyTitle("SCP1162-EXILED")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SCP1162-EXI-2.0")]
+[assembly: AssemblyProduct("SCP1162-EXILED")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
