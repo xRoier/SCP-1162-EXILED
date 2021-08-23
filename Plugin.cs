e7554223a171900aa47e3bab80ce2d5eab1c9d21
xRoier <58123692+xRoier@users.noreply.github.com>
2021-08-23 17:49:00 +0200

    Reversing ``Updated to EXILED 3.0``.

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,8 @@
 using System;
+using EvPlayer = Exiled.Events.Handlers.Player;
 using Exiled.API.Features;
 
-namespace SCP1162
+namespace SCP1162_EXI_2._0
 {
     public class Plugin : Plugin<Config>
     {
@@ -9,18 +10,18 @@ namespace SCP1162
         public override string Name => "SCP1162";
         public override string Author => "xRoier";
         public EventHandlers EventHandlers;
-        public override Version Version { get; } = new Version(2, 2, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
+        public override Version Version { get; } = new Version(2, 1, 4);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 13, 0);
         
         public override void OnEnabled()
         {
             EventHandlers = new EventHandlers(this);
-            Exiled.Events.Handlers.Player.DroppingItem += EventHandlers.OnItemDropped;
+            EvPlayer.ItemDropped += EventHandlers.OnItemDropped;
             base.OnEnabled();
         }
         public override void OnDisabled()
         {
-            Exiled.Events.Handlers.Player.DroppingItem -= EventHandlers.OnItemDropped;
+            EvPlayer.ItemDropped -= EventHandlers.OnItemDropped;
             EventHandlers = null;
             base.OnDisabled();
         }
