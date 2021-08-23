cba9fa40b3a477f234963476645b141209053fa2
xRoier <58123692+xRoier@users.noreply.github.com>
2021-08-23 17:48:29 +0200

    Reversing ``Updated to EXILED 3.0``.

--- Config.cs
+++ Config.cs
@@ -1,10 +1,10 @@
+using Exiled.API.Interfaces;
 using System.Collections.Generic;
 using System.ComponentModel;
-using Exiled.API.Interfaces;
 
-namespace SCP1162
+namespace SCP1162_EXI_2._0
 {
-    public class Config : IConfig
+	public class Config : IConfig
     {
         [Description("Is the plugin enabled?")]
         public bool IsEnabled { get; set; } = true;
@@ -16,36 +16,37 @@ namespace SCP1162
         public string ItemDropMessage { get; set; } = "<i>You try to drop the item through <color=yellow>SCP-1162</color> to get another...</i>";
         public ushort ItemDropMessageDuration { get; set; } = 5;
         [Description("The list of items.")]
-        public List<Exiled.API.Enums.ItemType> Chances { get; set; } = new List<Exiled.API.Enums.ItemType>
+        public List<ItemType> Chances { get; set; } = new List<ItemType>
         {
-            Exiled.API.Enums.ItemType.KeycardO5,
-            Exiled.API.Enums.ItemType.Scp500,
-            Exiled.API.Enums.ItemType.MicroHid,
-            Exiled.API.Enums.ItemType.KeycardNtfCommander,
-            Exiled.API.Enums.ItemType.KeycardContainmentEngineer,
-            Exiled.API.Enums.ItemType.Scp268,
-            Exiled.API.Enums.ItemType.GunCom15,
-            Exiled.API.Enums.ItemType.Scp207,
-            Exiled.API.Enums.ItemType.Adrenaline,
-            Exiled.API.Enums.ItemType.GunCom18,
-            Exiled.API.Enums.ItemType.KeycardFacilityManager,
-            Exiled.API.Enums.ItemType.Medkit,
-            Exiled.API.Enums.ItemType.KeycardNtfLieutenant,
-            Exiled.API.Enums.ItemType.KeycardGuard,
-            Exiled.API.Enums.ItemType.GrenadeHe,
-            Exiled.API.Enums.ItemType.KeycardZoneManager,
-            Exiled.API.Enums.ItemType.KeycardGuard,
-            Exiled.API.Enums.ItemType.Radio,
-            Exiled.API.Enums.ItemType.Ammo9X19,
-            Exiled.API.Enums.ItemType.Ammo12Gauge,
-            Exiled.API.Enums.ItemType.Ammo44Cal,
-            Exiled.API.Enums.ItemType.Ammo556X45,
-            Exiled.API.Enums.ItemType.Ammo762X39,
-            Exiled.API.Enums.ItemType.GrenadeFlash,
-            Exiled.API.Enums.ItemType.KeycardScientist,
-            Exiled.API.Enums.ItemType.KeycardJanitor,
-            Exiled.API.Enums.ItemType.Coin,
-            Exiled.API.Enums.ItemType.Flashlight
+            ItemType.KeycardO5,
+            ItemType.SCP500,
+            ItemType.MicroHID,
+            ItemType.KeycardNTFCommander,
+            ItemType.KeycardContainmentEngineer,
+            ItemType.SCP268,
+            ItemType.GunCOM15,
+            ItemType.GrenadeFrag,
+            ItemType.SCP207,
+            ItemType.Adrenaline,
+            ItemType.GunUSP,
+            ItemType.KeycardFacilityManager,
+            ItemType.Medkit,
+            ItemType.KeycardNTFLieutenant,
+            ItemType.KeycardSeniorGuard,
+            ItemType.Disarmer,
+            ItemType.KeycardZoneManager,
+            ItemType.KeycardScientistMajor,
+            ItemType.KeycardGuard,
+            ItemType.Radio,
+            ItemType.Ammo556,
+            ItemType.Ammo762,
+            ItemType.Ammo9mm,
+            ItemType.GrenadeFlash,
+            ItemType.WeaponManagerTablet,
+            ItemType.KeycardScientist,
+            ItemType.KeycardJanitor,
+            ItemType.Coin,
+            ItemType.Flashlight
         };
     }
 }
