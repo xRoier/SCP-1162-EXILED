5fb1a821ded4dd79657e4a175d1f0c9e77cc8e58
GabiRP <ruizperezgabriel@hotmail.com>
2021-08-24 21:25:40 +0200

    Fix NRE

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -12,6 +12,7 @@ namespace SCP1162
         {
             if (Vector3.Distance(ev.Player.Position, Exiled.API.Extensions.RoleExtensions.GetRandomSpawnProperties(RoleType.Scp173).Item1) <= 8.2f)
             {
+                if(ev.Item == null) return;
                 if (plugin.Config.UseHints)
                     ev.Player.ShowHint(plugin.Config.ItemDropMessage, plugin.Config.ItemDropMessageDuration);
                 else
