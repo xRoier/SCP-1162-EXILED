181ac1570bf15afcf8a7e600a2cd3ff96659a0f9
xRoier <rogerazomar@gmail.com>
2021-08-27 16:07:34 +0200

    Exiled trolling :troll:

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,7 +1,9 @@
 using Exiled.API.Features.Items;
 using Exiled.Events.EventArgs;
+using GameCore;
 using SCP1162.API;
 using UnityEngine;
+using Log = Exiled.API.Features.Log;
 
 namespace SCP1162
 {
@@ -12,17 +14,28 @@ namespace SCP1162
 
         public void OnItemDropped(DroppingItemEventArgs ev)
         {
-            if (ev.Item != null && Vector3.Distance(ev.Player.Position, Exiled.API.Extensions.RoleExtensions.GetRandomSpawnProperties(RoleType.Scp173).Item1) <= 8.2f)
+            try
             {
-                if (plugin.Config.UseHints)
-                    ev.Player.ShowHint(plugin.Config.ItemDropMessage, plugin.Config.ItemDropMessageDuration);
-                else
-                    ev.Player.Broadcast(plugin.Config.ItemDropMessageDuration, plugin.Config.ItemDropMessage, Broadcast.BroadcastFlags.Normal, true);
-                var oldItem = ev.Item.Base.ItemTypeId;
-                var newItem = plugin.Config.Chances[Random.Range(0, plugin.Config.Chances.Count)];
-                var eventArgs = new UsingScp1162EventArgs(ev.Player, newItem, oldItem);
-                Scp1162Event.OnUsingScp1162(eventArgs);
-                ev.Item = new Item(eventArgs.ItemAfter);
+                if (ev.Item != null && Vector3.Distance(ev.Player.Position, Exiled.API.Extensions.RoleExtensions.GetRandomSpawnProperties(RoleType.Scp173).Item1) <= 8.2f)
+                {
+                    if (plugin.Config.UseHints)
+                        ev.Player.ShowHint(plugin.Config.ItemDropMessage, plugin.Config.ItemDropMessageDuration);
+                    else
+                        ev.Player.Broadcast(plugin.Config.ItemDropMessageDuration, plugin.Config.ItemDropMessage, Broadcast.BroadcastFlags.Normal, true);
+                    ev.IsAllowed = false;
+                    var oldItem = ev.Item.Base.ItemTypeId;
+                    ev.Player.RemoveItem(ev.Item);
+                    var newItemType = plugin.Config.Chances[Random.Range(0, plugin.Config.Chances.Count)];
+                    var eventArgs = new UsingScp1162EventArgs(ev.Player, newItemType, oldItem);
+                    Scp1162Event.OnUsingScp1162(eventArgs);
+                    var newItem = new Item(eventArgs.ItemAfter);
+                    ev.Player.AddItem(newItem);
+                    ev.Player.DropItem(newItem);
+                }
+            }
+            catch
+            {
+                //Ignore, ev.Player.RemoveItem false positive.
             }
         }
     }
