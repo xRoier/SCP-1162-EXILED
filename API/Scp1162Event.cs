339d8f22df05a2ea995764d86c3d5779cc497e13
xRoier <rogerazomar@gmail.com>
2023-09-02 14:40:13 +0200

    EXILED 8.0.1

--- API/Scp1162Event.cs
+++ API/Scp1162Event.cs
@@ -1,11 +1,10 @@
-﻿using Exiled.Events;
-using Exiled.Events.Extensions;
+﻿using Exiled.Events.Features;
 
 namespace SCP1162.API
 {
     public static class Scp1162Event
     {
-        public static event Events.CustomEventHandler<UsingScp1162EventArgs> UsingScp1162;
-        public static void OnUsingScp1162(UsingScp1162EventArgs ev) => UsingScp1162.InvokeSafely(ev);
+        public static event CustomEventHandler<UsingScp1162EventArgs> UsingScp1162;
+        public static void OnUsingScp1162(UsingScp1162EventArgs ev) => UsingScp1162?.Invoke(ev);
     }
 }
--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,6 +1,5 @@
 using Exiled.API.Extensions;
 using Exiled.API.Features.Items;
-using Exiled.Events.EventArgs;
 using Exiled.Events.EventArgs.Player;
 using PlayerRoles;
 using SCP1162.API;
@@ -10,34 +9,27 @@ namespace SCP1162
 {
     public class EventHandlers
     {
-        private Plugin plugin;
-        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+        private readonly Plugin _plugin;
+        public EventHandlers(Plugin plugin) => _plugin = plugin;
 
         public void OnItemDropped(DroppingItemEventArgs ev)
         {
-            try
+            if(!ev.IsAllowed) return;
+            if (Vector3.Distance(ev.Player.Position, RoleTypeId.Scp173.GetRandomSpawnLocation().Position) <= 8.2f)
             {
-                if(!ev.IsAllowed) return;
-                if (Vector3.Distance(ev.Player.Position, RoleTypeId.Scp173.GetRandomSpawnLocation().Position) <= 8.2f)
-                {
-                    if (plugin.Config.UseHints)
-                        ev.Player.ShowHint(plugin.Config.ItemDropMessage, plugin.Config.ItemDropMessageDuration);
-                    else
-                        ev.Player.Broadcast(plugin.Config.ItemDropMessageDuration, plugin.Config.ItemDropMessage, Broadcast.BroadcastFlags.Normal, true);
-                    ev.IsAllowed = false;
-                    var oldItem = ev.Item.Base.ItemTypeId;
-                    ev.Player.RemoveItem(ev.Item);
-                    var newItemType = plugin.Config.Chances[Random.Range(0, plugin.Config.Chances.Count)];
-                    var eventArgs = new UsingScp1162EventArgs(ev.Player, newItemType, oldItem);
-                    Scp1162Event.OnUsingScp1162(eventArgs);
-                    var newItem = Item.Create(eventArgs.ItemAfter);
-                    ev.Player.AddItem(newItem);
-                    ev.Player.DropItem(newItem);
-                }
-            }
-            catch
-            {
-                //Ignore, ev.Player.RemoveItem false positive.
+                if (_plugin.Config.UseHints)
+                    ev.Player.ShowHint(_plugin.Config.ItemDropMessage, _plugin.Config.ItemDropMessageDuration);
+                else
+                    ev.Player.Broadcast(_plugin.Config.ItemDropMessageDuration, _plugin.Config.ItemDropMessage, Broadcast.BroadcastFlags.Normal, true);
+                ev.IsAllowed = false;
+                var oldItem = ev.Item.Base.ItemTypeId;
+                ev.Player.RemoveItem(ev.Item);
+                var newItemType = _plugin.Config.Chances[Random.Range(0, _plugin.Config.Chances.Count)];
+                var eventArgs = new UsingScp1162EventArgs(ev.Player, newItemType, oldItem);
+                Scp1162Event.OnUsingScp1162(eventArgs);
+                var newItem = Item.Create(eventArgs.ItemAfter);
+                ev.Player.AddItem(newItem);
+                ev.Player.DropItem(newItem);
             }
         }
     }
